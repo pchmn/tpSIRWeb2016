cce3c2f0a1856b8539f181181438e944d4c577cb
pchmn <paul.chemin35@gmail.com>
2016-02-03 15:37:02 +0100

    models.js

--- model.js
+++ model.js
@@ -1,3 +1,30 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.formes = {};
+};
+
+function Forme(couleur, epaisseur) {
+    this.couleur = couleur;
+    this.epaisseur = epaisseur;
+};
+
+function Rectangle(couleur, epaisseur, xInit, yInit, xFin, yFin) {
+    Forme.call(this.couleur, this.epaisseur);
+    this.largeur = Maths.abs(xFin - xInit);
+    this.hauteur = Maths.abs(yFin - yInit);
+    this.xOrigine = xInit;
+    this.yOrigine = yInit;
+};
+Rectangle.prototype = new Forme();
+
+function Line(couleur, epaisseur, xInit, yInit, xFin, yFin) {
+    Forme.call(this.couleur, this.epaisseur);
+    this.xDeb = xInit;
+    this.yDeb = yInit;
+    this.xFin = xFin;
+    this.yFin = yFin;
+};
+Line.prototype = new Forme();
